ef3002a721eaded1d9a093078e273572745565ae
rohana-2005 <163023631+rohana-2005@users.noreply.github.com>
2025-06-23 11:30:45 +0530

    Add files via upload

---
+++ sans_sent_game.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify, send_file
+from flask_cors import CORS
+import json
+import random
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS
+
+# Load sentences data
+with open('sentences.json', 'r', encoding='utf-8') as f:
+    sentences = json.load(f)
+
+@app.route('/')
+def serve_game():
+    return send_file('game.html')
+
+@app.route('/get_random_sentence')
+def get_random_sentence():
+    sentence = random.choice(sentences)
+    return jsonify({
+        'sentence': sentence['sentence'],
+        'subject': sentence['subject']['form'] if sentence['subject'] else None,
+        'object': sentence['object']['form'] if sentence['object'] else None,
+        'verb': sentence['verb']['form'],
+        'hint': {
+            'subject': {
+                'gender': sentence['subject']['gender'] if sentence['subject'] else None,
+                'number': sentence['subject']['number'] if sentence['subject'] else None
+            } if sentence['subject'] else None,
+            'object': {
+                'gender': sentence['object']['gender'] if sentence['object'] else None,
+                'number': sentence['object']['number'] if sentence['object'] else None
+            } if sentence['object'] else None,
+            'verb': {
+                'person': sentence['verb']['person'],
+                'number': sentence['verb']['number'],
+                'class': sentence['verb']['class'],
+                'meaning': sentence['verb']['meaning']
+            }
+        }
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
